5a4a86e9f4b97c60916c0bee9ce4edf53112db07
cacarico <caio.quinilato@gmail.com>
2026-03-08 19:34:01 +0100

    fix: remove old tests

--- tests/run_all_tests.lua
+++
@@ -1,9 +0,0 @@
--- tests/run_all_tests.lua
-local luaunit = require("luaunit")
-
--- Require all individual test modules
-require("test_monitors")
--- require('test_another')  -- Add more tests as needed
-
--- Execute all tests
-os.exit(luaunit.LuaUnit.run())
